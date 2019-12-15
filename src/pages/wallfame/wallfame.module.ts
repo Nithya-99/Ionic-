279e87d8d7aac3e2aaa01beec59eb91692624ede
Rahul-22 <47082978+Rahul-22@users.noreply.github.com>
2019-12-16 00:33:34 +0530

    created walloffame

---
+++ src/pages/wallfame/wallfame.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { WallfamePage } from './wallfame';
+
+@NgModule({
+  declarations: [
+    WallfamePage,
+  ],
+  imports: [
+    IonicPageModule.forChild(WallfamePage),
+  ],
+})
+export class WallfamePageModule {}
