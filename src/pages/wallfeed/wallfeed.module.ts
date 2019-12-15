d906bb7751ed464b1c2c6dfc58dded6f86f0af21
Rahul-22 <47082978+Rahul-22@users.noreply.github.com>
2019-12-16 00:32:48 +0530

    wallfeed half

---
+++ src/pages/wallfeed/wallfeed.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { WallfeedPage } from './wallfeed';
+
+@NgModule({
+  declarations: [
+    WallfeedPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(WallfeedPage),
+  ],
+})
+export class WallfeedPageModule {}
